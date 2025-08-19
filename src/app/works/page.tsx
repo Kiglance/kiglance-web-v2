bc11a5257966680a4b9454aa4418516c6d872451
wayneleon1 <rurangwaleon10@gmail.com>
2025-08-19 09:47:36 +0200

    Intergrate React Count package

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,41 +1,13 @@
 import Footer from '@/components/Footer';
-import Navbar from '@/components/Navbar';
 import ProjectCard from '@/components/ProjectCard';
 import { SectionHeader } from '@/components/SectionHeader';
 import Button from '@/components/ui/Button';
 import { ArrowRight } from 'lucide-react';
-import React from 'react';
 import { projects } from '../../../data/static';
 
-interface InfoStatics {
-  label: string;
-  value: number;
-  type: 'number' | 'percentage';
-}
+import CountContainer from '@/components/CountContainer';
 
 const page = () => {
-  const infoStatics: InfoStatics[] = [
-    {
-      label: 'Projects Completed',
-      value: 20,
-      type: 'number',
-    },
-    {
-      label: 'Years Experience',
-      value: 8,
-      type: 'number',
-    },
-    {
-      label: 'Client Satisfaction',
-      value: 98,
-      type: 'percentage',
-    },
-    {
-      label: 'Expert Developer',
-      value: 5,
-      type: 'number',
-    },
-  ];
   return (
     <>
       <div className="relative overflow-hidden">
@@ -50,23 +22,7 @@ const page = () => {
                 design with robust development.
               </p>
             </div>
-            <div className="rounded-2xl bg-[#e2e1e1] px-4 py-6 sm:px-8 sm:py-5 md:px-12 lg:px-14 dark:bg-[#222222]">
-              <div className="flex flex-col gap-6 sm:flex-row sm:items-center sm:justify-between sm:gap-4">
-                {infoStatics.map((item, idx) => {
-                  return (
-                    <div key={idx} className="flex-1 text-center">
-                      <p className="text-primary text-3xl font-semibold sm:text-4xl lg:text-5xl">
-                        {item.value}
-                        <span>{item.type === 'number' ? '+' : '%'}</span>
-                      </p>
-                      <p className="text-neutral-1-light mt-1 text-sm sm:text-base lg:text-lg dark:text-white">
-                        {item.label}
-                      </p>
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
+            <CountContainer />
           </div>
         </div>
       </div>
