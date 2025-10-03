f34d84a0c40214da7e5cb57407fef55c868e6fe6
wayneleon1 <rurangwaleon10@gmail.com>
2025-10-03 16:51:59 +0200

    Added dynamic metadata on slug project page

--- src/app/works/[id]/page.tsx
+++ src/app/works/[id]/page.tsx
@@ -1,10 +1,37 @@
 import { Metadata } from 'next';
 import ProjectClientPage from './projectClientPage';
+import { projects } from '../../../../data/static';
 
-export const metadata: Metadata = {
-  title: 'Project Details',
-  description: 'Detailed view of the selected project',
-};
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const projectId = params.id;
+
+  const project = projects.find((proj) => proj.id === Number(projectId));
+
+  if (!project) {
+    return {
+      title: {
+        absolute: 'Kiglance',
+      },
+    };
+  }
+
+  return {
+    title: project.title,
+    description: project.description,
+    openGraph: {
+      title: project.title,
+      description: project.description,
+      images: [
+        {
+          url: project.image,
+          width: 1200,
+          height: 630,
+          alt: project.title,
+        },
+      ],
+    },
+  };
+}
 
 const page = () => {
   return <ProjectClientPage />;
